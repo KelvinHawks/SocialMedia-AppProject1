5b0500afc1bff14dd0e0f547da896327238c4402
kelvin <kevohkyme35@gmail.com>
2023-09-17 22:34:15 +0300

    Add authentication form

--- client/src/users/pages/Auth.js
+++ client/src/users/pages/Auth.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
-import { VALIDATOR_MINLENGTH, VALIDATOR_EMAIL } from '../../shared/util/validators'
+import { VALIDATOR_MINLENGTH, VALIDATOR_EMAIL, VALIDATOR_REQUIRE} from '../../shared/util/validators'
 import { useForm } from '../../shared/hooks/form-hook'
 import Card from '../../shared/UIElements/Card'
 import './Auth.css'
 function Auth() {
-    const[formState, inputHandler]=useForm({
+    const[isLoginMode, setIsLoginMode] = useState(true)
+    const[formState, inputHandler, setFormData]=useForm({
         email:{
             value:'',
             isValid:false
@@ -20,11 +21,39 @@ function Auth() {
         e.preventDefault()
         console.log(formState.inputs);
     }
+    const switchModeHandler = ()=>{
+        if(!isLoginMode){
+            setFormData({
+                ...formState.inputs,
+                name:undefined
+            }, formState.inputs.email.isValid && formState.inputs.password.isValid)
+        }else{
+            setFormData({
+                ...formState.inputs,
+                name:{
+                    value:'',
+                    isValid:false
+                }
+            },false)
+        }
+        setIsLoginMode(prevMode=>!prevMode)
+    }
   return (
     <Card className='authentication'>
         <h2>Login required</h2>
         <hr/>
         <form onSubmit={authSubmitHandler}>
+            {!isLoginMode && 
+            <Input
+                id='name'
+                element='input'
+                type='text'
+                label='Username'
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText='Username required'
+                onInput={inputHandler}
+            />
+            }
             <Input
                 id='email'
                 element='input'
@@ -43,8 +72,11 @@ function Auth() {
                 errorText='Password must contain 8 characters'
                 onInput={inputHandler}
             />
-            <Button type='submit' disabled={!formState.isValid}>LOGIN</Button>
+            <Button type='submit' disabled={!formState.isValid}>
+                {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+            </Button>
         </form>
+        <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
     </Card>
   )
 }
