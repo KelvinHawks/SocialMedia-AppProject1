00a46e179ad4d1a631ea6dadf642779ac57609c4
kelvin <kevohkyme35@gmail.com>
2023-10-02 18:31:11 +0300

    Handle errors

--- server/app.js
+++ server/app.js
@@ -7,6 +7,13 @@ const placesRoutes = require("./routes/places-routes");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api/places", placesRoutes);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured" });
+});
 app.listen(5000, () => {
   console.log("app listening on port 5000");
 });
--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const HttpError = require("../models/http-error");
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -17,18 +17,26 @@ const DUMMY_PLACES = [
 ];
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-  if (place) {
-    res.json({ title: place.title, description: place.description });
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided placeId", 404);
   } else {
-    res.json({ message: "Error" });
+    res.json({ place });
   }
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
-  res.json({ place });
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  if (!place) {
+    throw new Error("Could not find a place for the provided userId", 404);
+  } else {
+    res.json({ place });
+  }
 });
 
 module.exports = router;
