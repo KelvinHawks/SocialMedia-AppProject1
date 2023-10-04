44fc9baa8c7edbe01f864f1d2722e7bb9732f5d3
kelvin <kevohkyme35@gmail.com>
2023-10-04 22:52:24 +0300

    Add routes

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,42 +1,15 @@
 const express = require("express");
-const HttpError = require("../models/http-error");
+const placeControllers = require("../controllers/places-controller");
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire state building",
-    description: "One of the most beautiful sky scrappers",
-    location: {
-      lat: 50,
-      lng: 50,
-    },
-    address: "20w",
-    creator: "u1",
-  },
-];
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided placeId", 404);
-  } else {
-    res.json({ place });
-  }
-});
+router.get("/:pid", placeControllers.getPlaceById);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  if (!place) {
-    throw new Error("Could not find a place for the provided userId", 404);
-  } else {
-    res.json({ place });
-  }
-});
+router.get("/user/:uid", placeControllers.getPlaceByUserId);
+
+router.post("/", placeControllers.createPlace);
+
+router.patch("/:pid", placeControllers.updatePlace);
+
+router.delete("/:pid", placeControllers.deletePlace);
 
 module.exports = router;
