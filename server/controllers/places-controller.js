ad3f2ae950a1757c426aa59bb3532e2bf7031807
kelvin <kevohkyme35@gmail.com>
2023-10-09 09:30:59 +0300

    Add location address

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -1,6 +1,7 @@
 const uuid = require("uuid");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
+const getCoordsForAddress = require("../util/location");
 
 const uniqueId = uuid.v4();
 let DUMMY_PLACES = [
@@ -52,15 +53,22 @@ const getPlacesByUserId = (req, res, next) => {
   }
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
 
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
   }
-  const { title, description, coordinates, address, creator } = req.body;
-
+  const { title, description, address, creator } = req.body;
+  let coordinates;
+  try {
+    coordinates = getCoordsForAddress();
+  } catch (error) {
+    return next(error);
+  }
   const createdPlace = {
     id: uniqueId,
     title,
